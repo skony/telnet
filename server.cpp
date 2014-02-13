868af025b2c9dac91a85c23c1163600b9733b090
skony <piotr.skon@gmail.com>
2014-02-14 00:18:24 +0100

    phtread instead of fork

--- server.cpp
+++ server.cpp
@@ -9,18 +9,28 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
-typedef struct User
+#define NUM_THREADS 100
+
+struct User
 {
   struct sockaddr_in user_addr;
   char pass[];
 } User;
 
-User users[10];
-int users_count = 0;
-struct sockaddr_in serv_addr, cli_addr;
-int sockfd, newsockfd, portno = 5001, clilen;
+struct thread_data
+{
+	unsigned int s_addr;
+	int newsockfd;
+} thread_data;
 
-void addHost()
+struct User users[10];
+int users_count = 0;
+int threads = 0;
+struct sockaddr_in serv_addr;
+int sockfd, portno = 5001;
+struct thread_data thread_data_array[NUM_THREADS];
+
+void addHost(int newsockfd, unsigned int s_addr)
 {
   int n;
   char buffer[256];
@@ -29,8 +39,10 @@ void addHost()
   while(1)
   {
     char msg[50] = "Please write password";
-    n = write(newsockfd, msg, strlen(msg));
-    n = read(newsockfd,buffer,255);
+    write(newsockfd, msg, strlen(msg));
+		bzero(buffer, 256);
+		bzero(buffer2, 256);
+    read(newsockfd,buffer,255);
     strcpy(msg, "Please write password again");
     write(newsockfd, msg, strlen(msg));
     read(newsockfd,buffer2,255);
@@ -41,11 +53,11 @@ void addHost()
     if(strcmp(buffer, buffer2) == 0)
     {
       printf("debug: adding1\n");
-      users[users_count].user_addr.sin_family = cli_addr.sin_family;
+      users[users_count].user_addr.sin_family = AF_INET;
       printf("debug: adding2\n");
-      users[users_count].user_addr.sin_addr = cli_addr.sin_addr;
+      users[users_count].user_addr.sin_addr.s_addr = s_addr;
       printf("debug: adding3\n");
-      users[users_count].user_addr.sin_port = cli_addr.sin_port;
+      users[users_count].user_addr.sin_port = htons(portno);
       printf("debug: adding4\n");
       strcpy(users[users_count].pass, buffer);
       printf("debug: adding5\n");
@@ -93,13 +105,18 @@ int connectToHost(char host[], struct sockaddr_in &dest_addr, bool &connection)
 	}
 }
 
-void doprocessing (int sock)
+void* doprocessing (void *arg)
 {
-  int n;
+  int n, sock_dest, newsockfd;
   char buffer[256];
 	bool connected = false;
 	struct sockaddr_in dest_addr;
-	int sock_dest;
+	unsigned int s_addr;
+	struct thread_data *my_data;
+
+	my_data = (struct thread_data *) arg;
+	newsockfd = my_data->newsockfd;
+	s_addr = my_data->s_addr;
 
 	while(1)
 	{
@@ -110,7 +127,7 @@ void doprocessing (int sock)
 		if(strcmp(buffer, "-host") == 0)
 		{
 		  printf("debug: addhost\n");
-		  addHost();
+		  addHost(newsockfd, s_addr);
 		}
 		else if(!connected)
 		{
@@ -122,9 +139,10 @@ void doprocessing (int sock)
 
 int main( int argc, char *argv[] )
 {
-	int pid;
+	int pid, t;
   char buffer[256];
-  int  n;
+  int  n, newsockfd, clilen;
+	struct sockaddr_in cli_addr;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -151,21 +169,13 @@ int main( int argc, char *argv[] )
 
   while(1)
   {
-
     newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t*) &clilen);
-    pid = fork();
-
-    if (pid == 0)  
-    {
-      /* This is the client process */
-      close(sockfd);
-      doprocessing(newsockfd);
-      exit(0);
-    }
-    else
-    {
-      close(newsockfd);
-    }
+		t = threads;
+		thread_data_array[t].s_addr = cli_addr.sin_addr.s_addr;
+		thread_data_array[t].newsockfd = newsockfd;
+    pthread_t pid;
+		pthread_create(&pid, NULL, doprocessing, &thread_data_array[t]);
+		threads = threads + 1;
   }
 	return 0;
 
